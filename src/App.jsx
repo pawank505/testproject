6c6f31e18ae124b8ed7b73ee71ef52fc77d8d8b7
pawank505 <pawankusekar505@gmail.com>
2026-05-16 17:37:22 +0530

    SCRUM-5 routes changed [auto-pr, taskcompleted, main]

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/stockform" element={<StockForm />} />
         {/* Add more routes here */}
       </Routes>
