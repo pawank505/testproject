f557adbc6832703f47871789c7c60c6631e97c92
pawank505 <pawankusekar505@gmail.com>
2026-06-05 12:44:32 +0530

    SCRUM-29 remove navbar div  [auto-pr, taskcompleted, main]

--- src/components/Stockform.jsx
+++ src/components/Stockform.jsx
@@ -17,10 +17,6 @@ const Stockform = () => {
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Close the scale test
         </button>
-        <div>
-          Navbar is added to the project
-        </div>
-
       </div>
     </div>
   );
